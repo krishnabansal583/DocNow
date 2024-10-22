b7cc0cb739c0b98ad12701e787f41b8c5ed58aa3
krishna <krishnabansal583@gmail.com>
2024-10-22 14:42:59 +0530

    making front page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header.JSX'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
+
+
+const Home = () => {
+  return (
+   <>
+   <Header/>
+   <SpecialityMenu/>
+   <TopDoctors/>
+   </>
+   
+  )
+}
+
+export default Home
